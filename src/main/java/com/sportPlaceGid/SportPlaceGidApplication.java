ab7641e42fc881dda55f0f439f8a659dddbd2079
Sadykh <sadykh.sadykhov@ya.ru>
2020-01-13 15:41:16 +0300

    Add base sport place gid application and help information

---
+++ src/main/java/com/sportPlaceGid/SportPlaceGidApplication.java
@@ -0,0 +1,13 @@
+package com.sportPlaceGid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportPlaceGidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportPlaceGidApplication.class, args);
+	}
+
+}
